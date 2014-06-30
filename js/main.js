aff126a597e0270acd48a00a77dff42945a95fea
Sebastian Sandqvist <seb@sparque.me>
2014-06-30 14:02:41 -0600

    Part 1 functional

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 // callback function
 var setFlag = function(event) {
-	var x = event.pageX;
-	var y = event.pageY;
+	x = ((event.pageX)/width)*100;
+	y = ((event.pageY)/height)*100;
+	console.log(x,y)
 	var flag = $('<div>');
 	flag.addClass('marker');
 	flag.css({
-		top: y,
-		left: x
+		top: y + '%',
+		left: x + '%'
 	});
 	$('.container').append(flag);
 }
 
+var width = $('.container').width();
+var height = $('.container').height();
+var x, y;
+
 $(document).on('ready', function() {
-  $('.topo-map').on('click', setFlag)
+  $('.container').on('click', setFlag)
+  $(window).on('resize', function() {
+  	width = $('.container').width();
+  	height = $('.container').height();
+  })
 });
